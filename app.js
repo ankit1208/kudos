cfb8da442b7d206d0318b4ccdcf0099559ace3d3
ankit1208 <58726499+ankit1208@users.noreply.github.com>
2021-06-15 17:49:29 +0530

    filter api

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
-// fetch('https://api.spacexdata.com/v3/launches?limit=100')
-//     .then(res=>res.json())
-//     .then(data=>console.log(data))
+fetch('https://api.spacexdata.com/v3/launches?limit=100')
+    .then(res=>res.json())
+    .then(data=>{
+        document.querySelector('#first').innerHTML="<b>Mission Id :</b>"+data[0].flight_number;
+    })
+
