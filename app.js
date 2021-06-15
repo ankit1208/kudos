54972e30f7b019a5f41bac61f1803ebac559f273
ankit1208 <58726499+ankit1208@users.noreply.github.com>
2021-06-16 01:51:20 +0530

    position

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 fetch('https://api.spacexdata.com/v3/launches?limit=100')
     .then(res=>res.json())
     .then(data=>{
-        document.querySelector('#first').innerHTML="<b>Mission Id :</b>"+data[0].flight_number;
+        for(var i=data.length-1;i>=0;i--){
+            console.log(i)
+            var datain=document.createElement('div')
+            datain.className = "data1";
+            var target= document.getElementById('targetrow'); 
+            target.insertAdjacentElement("afterbegin", datain); 
+            var newdata=document.querySelector('.data1')
+            newdata.innerHTML="<b>Mission Id :</b>"+data[i].flight_number;
+        }
+        // document.querySelector('#first').innerHTML="<b>Mission Id :</b>"+data[0].flight_number;
     })
 
